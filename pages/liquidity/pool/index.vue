3632a9b87f49fdf4e52f5d25cdbcb3fa6f6e8658
thqt1102 <thqt1102@gmail.com>
2025-02-27 17:23:17 +0700

    feat: implement BaseTab component and integrate it into liquidity pool page

--- pages/liquidity/pool/index.vue
+++ pages/liquidity/pool/index.vue
@@ -11,12 +11,13 @@
         </p>
       </div>
       <div class="flex items-center gap-[50px]">
-        <div class="flex gap-6 text-sm font-semibold text-gray-7">
-          <span class="cursor-pointer pb-[10px] hover:text-hyperlink" :class="{ 'tab-active': tabActive === 'ALL' }" @click="tabActive = 'ALL'">All Pools</span>
-          <span class="cursor-pointer pb-[10px] hover:text-hyperlink" :class="{ 'tab-active': tabActive === 'POSITION' }" @click="tabActive = 'POSITION'"
-            >My Positions</span
-          >
-        </div>
+        <BaseTab
+          v-model:model="tabActive"
+          :list="[
+            { title: 'All Pools', value: 'ALL' },
+            { title: 'My Position', value: 'POSITION' }
+          ]"
+        />
         <NuxtLink to="/liquidity/add">
           <BaseButton size="sm" class="flex w-[149px] items-center gap-1 !text-white">
             <BaseIcon name="plus" size="24" />
@@ -71,20 +72,4 @@
   init()
 </script>
 
-<style lang="scss" scoped>
-  .tab-active {
-    position: relative;
-    color: var(--color-hyperlink);
-    &::before {
-      content: '';
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      width: 32px;
-      height: 4px;
-      border-radius: 2px;
-      background-color: #1573fe;
-    }
-  }
-</style>
+<style lang="scss" scoped></style>
