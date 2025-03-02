035eadef426b194fbb30e99a4931070cec883a70
thqt1102 <thqt1102@gmail.com>
2025-03-02 21:39:04 +0700

    feat: update ListFee component to conditionally render based on token selection

--- components/liquidity/ListFee.vue
+++ components/liquidity/ListFee.vue
@@ -2,28 +2,57 @@
   <div class="mt-8">
     <div class="flex items-center justify-between">
       <h4 class="text-lg font-semibold leading-7">Fee tier</h4>
-      <BaseLoadingButton v-if="isPendingAll" />
+      <BaseLoadingButton v-if="isPendingAll && isToken0Selected && isToken1Selected" />
     </div>
-    <div v-if="!isPendingAll" class="mt-4 grid grid-cols-[repeat(4,100px)] gap-5">
-      <div
-        v-for="(item, index) in LIST_FEE_AMOUNT"
-        :key="item.value"
-        class="flex cursor-pointer flex-col items-center gap-5 rounded-lg border border-solid border-gray-3 bg-[#fafafa] px-[13px] pb-[26px] pt-[15px]"
-        @click="handleSelectFee(item)"
-      >
-        <div class="flex items-center gap-[6px]">
-          <BaseIcon v-show="feeAmount === item.value" name="radio-fill" size="24" />
-          <BaseIcon v-show="feeAmount !== item.value" name="radio" size="24" />
-          <span class="text-sm font-bold leading-5">{{ item.value / 10000 }}%</span>
+    <template v-if="isToken0Selected && isToken1Selected">
+      <div v-if="!isPendingAll" class="mt-4 grid grid-cols-[repeat(4,100px)] gap-5">
+        <div
+          v-for="(item, index) in LIST_FEE_AMOUNT"
+          :key="item.value"
+          class="flex cursor-pointer flex-col items-center gap-5 rounded-lg border border-solid border-gray-3 bg-[#fafafa] px-[13px] pb-[26px] pt-[15px]"
+          @click="handleSelectFee(item)"
+        >
+          <div class="flex items-center gap-[6px]">
+            <BaseIcon v-show="feeAmount === item.value" name="radio-fill" size="24" />
+            <BaseIcon v-show="feeAmount !== item.value" name="radio" size="24" />
+            <span class="text-sm font-bold leading-5">{{ item.value / 10000 }}%</span>
+          </div>
+          <p class="text-center text-xs text-gray-8">{{ listPoolExits[index] ? 'Created' : 'Not created' }}</p>
         </div>
-        <p class="text-center text-xs text-gray-8">{{ listPoolExits[index] ? 'Created' : 'Not created' }}</p>
       </div>
-    </div>
+    </template>
+    <template v-else>
+      <div class="mt-4 grid grid-cols-[repeat(4,100px)] gap-5">
+        <div
+          v-for="(item, index) in LIST_FEE_AMOUNT"
+          :key="item.value"
+          class="flex cursor-pointer flex-col items-center gap-5 rounded-lg border border-solid border-gray-3 bg-[#fafafa] px-[13px] pb-[26px] pt-[15px]"
+          @click="handleSelectFee(item)"
+        >
+          <div class="flex items-center gap-[6px]">
+            <BaseIcon v-show="feeAmount === item.value" name="radio-fill" size="24" />
+            <BaseIcon v-show="feeAmount !== item.value" name="radio" size="24" />
+            <span class="text-sm font-bold leading-5">{{ item.value / 10000 }}%</span>
+          </div>
+          <p class="text-center text-xs text-gray-8">{{ listPoolExits[index] ? 'Created' : 'Not created' }}</p>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { LIST_FEE_AMOUNT } from '~/constant'
+
+  interface IProps {
+    isToken0Selected: boolean
+    isToken1Selected: boolean
+  }
+
+  const _props = withDefaults(defineProps<IProps>(), {
+    isToken0Selected: false,
+    isToken1Selected: false
+  })
 
   const { feeAmount, baseCurrency, quoteCurrency, leftRangeTypedValue, rightRangeTypedValue } = storeToRefs(useLiquidityStore())
   const { resetFiled } = useLiquidityStore()
