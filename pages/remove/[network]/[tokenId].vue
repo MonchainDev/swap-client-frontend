504da3ac6b5485e7d57c2f86bd19f4192fd9694b
thqt1102 <thqt1102@gmail.com>
2025-03-12 10:28:49 +0700

    feat: add composable to fetch V3 token IDs by account and integrate with remove page

--- pages/remove/[network]/[tokenId].vue
+++ pages/remove/[network]/[tokenId].vue
@@ -138,7 +138,7 @@
 <script lang="ts" setup>
   import { CurrencyAmount, Percent } from '@monchain/swap-sdk-core'
   import { useAccount } from '@wagmi/vue'
-  import { hexToBigInt } from 'viem'
+  import { hexToBigInt, type Address } from 'viem'
   import { BIPS_BASE } from '~/constant'
   import { CONTRACT_ADDRESS } from '~/constant/contract'
   import { sendTransaction, waitForTransactionReceipt } from '@wagmi/core'
@@ -147,6 +147,7 @@
   import { NonfungiblePositionManager } from '~/utils/nonfungiblePositionManager'
   import { WNATIVE } from '~/constant/token'
   import Decimal from 'decimal.js'
+  import { MasterChefV3 } from '~/utils/masterChefV3'
 
   definePageMeta({
     middleware: ['reset-form-liquidity-middleware', 'reset-all-popup-middleware']
@@ -171,6 +172,10 @@
   // const nativeWrappedSymbol = computed(() => nativeCurrency.value?.wrapped.symbol)
 
   const { position } = useV3PositionsFromTokenId(tokenId.value)
+
+  const { tokenIds } = useV3TokenIdsByAccount(CONTRACT_ADDRESS.MASTER_CHEF_V3 as Address)
+
+  const isStakeMV3 = computed(() => tokenIds.value?.includes(tokenId.value!))
 
   const loading = computed(() => !positionSDK.value)
 
@@ -261,9 +266,12 @@
       const deadline = Math.floor(Date.now() / 1000) + 5 * 60 // 5 minutes
       const allowedSlippage = 50
 
+      const interfaceManager = isStakeMV3.value ? MasterChefV3 : NonfungiblePositionManager
+      const addressTo = (isStakeMV3.value ? CONTRACT_ADDRESS.MASTER_CHEF_V3 : CONTRACT_ADDRESS.NONFUNGIBLE_POSITION_MANAGER) as `0x${string}`
+
       // we fall back to expecting 0 fees in case the fetch fails, which is safe in the
       // vast majority of cases
-      const { calldata, value } = NonfungiblePositionManager.removeCallParameters(positionSDK.value, {
+      const { calldata, value } = interfaceManager.removeCallParameters(positionSDK.value, {
         tokenId: tokenId.value!,
         liquidityPercentage: liquidityPercentage.value,
         slippageTolerance: basisPointsToPercent(allowedSlippage),
@@ -279,7 +287,7 @@
       console.log('🚀 ~ handleRemove ~ calldata:', calldata)
 
       const txHash = await sendTransaction(config, {
-        to: CONTRACT_ADDRESS.NONFUNGIBLE_POSITION_MANAGER as `0x${string}`,
+        to: addressTo,
         data: calldata,
         value: hexToBigInt(value),
         account: account.value
