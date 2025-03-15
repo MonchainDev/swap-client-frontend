f5016caca86d9dc63f5023e4a17b0c96f14967c4
thqt1102 <thqt1102@gmail.com>
2025-03-15 10:46:59 +0700

    fix: normalize token and address fields in postTransaction utility

--- utils/postTransaction.ts
+++ utils/postTransaction.ts
@@ -2,7 +2,16 @@ import { EncryptRequestType, type IBodyTxCollect } from '~/types/encrypt.type'
 
 export default async function postTransaction(body: IBodyTxCollect) {
   const { $buildEncryptedBody } = useNuxtApp()
+  const data: IBodyTxCollect = {
+    ...body,
+    fromToken: body.fromToken?.toLowerCase(),
+    toToken: body.toToken?.toLowerCase(),
+    poolAddress: body.poolAddress?.toLowerCase(),
+    fromAddress: body.fromAddress?.toLowerCase(),
+    toAddress: body.toAddress?.toLowerCase(),
+    transactionHash: body.transactionHash?.toLowerCase()
+  }
   const initSeconds = Math.round((new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000) / 1000)
-  const encryptedBody = $buildEncryptedBody(EncryptRequestType.TX_COLLECT, { initSeconds, data: body })
+  const encryptedBody = $buildEncryptedBody(EncryptRequestType.TX_COLLECT, { initSeconds, data })
   await $fetch('/api/transaction/collect', { body: JSON.stringify(encryptedBody), method: 'POST' })
 }
