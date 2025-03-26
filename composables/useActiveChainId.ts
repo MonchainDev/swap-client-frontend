5dfc2afb945c8188020c1586f538ed8a01f58801
thqt1102 <thqt1102@gmail.com>
2025-03-26 10:17:53 +0700

    refactor: get network params on route in useActiveChainId

--- composables/useActiveChainId.ts
+++ composables/useActiveChainId.ts
@@ -1,10 +1,24 @@
 import { useAccount } from '@wagmi/vue'
+import { LIST_NETWORK } from '~/config/networks'
 // import { ChainId } from '~/types'
 
 export default function useActiveChainId() {
-  const { chainId } = useAccount()
+  /**
+   * REQUIRED: Params in route has network in case of using network in url such as page detail position, remove liquidity, etc
+   * If not, use chainId from account
+   */
+  const { chainId: chain, isConnected } = useAccount()
 
-  // const defaultChainId = computed(() => ChainId.MON_TESTNET)
+  const route = useRoute()
 
-  return { chainId }
+  const urlIncludeNetwork = computed(() => ('network' in route.params ? route.params.network : undefined))
+
+  const networkUsed = computed(() => {
+    if (isConnected.value) {
+      return chain.value
+    }
+    return LIST_NETWORK.find((network) => network.network === urlIncludeNetwork.value?.toUpperCase())?.chainId
+  })
+
+  return { chainId: networkUsed }
 }
