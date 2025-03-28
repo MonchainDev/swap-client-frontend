2f71bf8bae5c24f5f349de64d22cf2fda11a36e8
thqt1102 <thqt1102@gmail.com>
2025-03-28 21:24:09 +0700

    fix: update BSC_TESTNET RPC URL to use public node

--- config/nodes.ts
+++ config/nodes.ts
@@ -5,7 +5,7 @@ import { ChainId } from '~/types'
 
 export const PUBLIC_NODES: Record<ChainId, string[] | readonly string[]> = {
   [ChainId.MON_TESTNET]: ['https://rpc-testnet.monchain.info'],
-  [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+  [ChainId.BSC_TESTNET]: ['https://bsc-testnet-rpc.publicnode.com'],
   [ChainId.SEPOLIA]: sepolia.rpcUrls.default.http,
   [ChainId.ARBITRUM_SEPOLIA]: arbitrumSepolia.rpcUrls.default.http,
   [ChainId.AMOY_POLYGON]: ['https://rpc-amoy.polygon.technology/']
