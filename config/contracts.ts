a67b9119e191329457b6a3a45d224e80d7310e48
aniadev9 <aniadev.99@gmail.com>
2025-04-01 23:47:42 +0700

    chore: update contract addresses for MON_TESTNET and other chains

--- config/contracts.ts
+++ config/contracts.ts
@@ -3,7 +3,7 @@ import { ChainId } from '~/types'
 import type { Address, Hash } from 'viem'
 
 /** DEPLOYER_ADDRESSES */
-const DEPLOYER_ADDRESSES_BASE = '0xF9012437655F666bcB07f50Cc471f531629342Df'
+const DEPLOYER_ADDRESSES_BASE = '0xEFBA107c5ede093f6A0CfE3E53acf2715D94E32e'
 const DEPLOYER_ADDRESSES = {
   [ChainId.MON_TESTNET]: DEPLOYER_ADDRESSES_BASE,
   [ChainId.AMOY_POLYGON]: '0x',
@@ -13,9 +13,9 @@ const DEPLOYER_ADDRESSES = {
 } as const satisfies Record<ChainId, Address>
 
 /** POOL_INIT_CODE_HASH_BASE */
-const POOL_INIT_CODE_HASH_BASE = '0xa415940637fe3d498bed0c48dfd1c0203e8caea5513e272a3749a36063a9582f'
+const POOL_INIT_CODE_HASH_BASE = '0xec014d553cf8e227ff815e13561fc490e9f91efce2d91baf2293cbd6a7ba98cb'
 const POOL_INIT_CODE_HASHES = {
-  [ChainId.MON_TESTNET]: POOL_INIT_CODE_HASH_BASE,
+  [ChainId.MON_TESTNET]: '0xa415940637fe3d498bed0c48dfd1c0203e8caea5513e272a3749a36063a9582f',
   [ChainId.AMOY_POLYGON]: '0x',
   [ChainId.BSC_TESTNET]: POOL_INIT_CODE_HASH_BASE,
   [ChainId.SEPOLIA]: '0x',
@@ -23,7 +23,7 @@ const POOL_INIT_CODE_HASHES = {
 } as const satisfies Record<ChainId, Hash>
 
 /** NFT_POSITION_MANAGER_ADDRESS */
-const NFT_POSITION_MANAGER_ADDRESS_BASE = '0xD1D4B9D291DDbDCa04349A7De6278db723b645b9'
+const NFT_POSITION_MANAGER_ADDRESS_BASE = '0x3beEDf6cE77CBdD85f04E1B5Cdd82c04d853b910'
 const NFT_POSITION_MANAGER_ADDRESSES = {
   [ChainId.MON_TESTNET]: NFT_POSITION_MANAGER_ADDRESS_BASE,
   [ChainId.AMOY_POLYGON]: '0x',
@@ -32,7 +32,7 @@ const NFT_POSITION_MANAGER_ADDRESSES = {
   [ChainId.ARBITRUM_SEPOLIA]: '0x'
 } as const satisfies Record<ChainId, Address>
 
-const FACTORY_ADDRESS_BASE = '0x40e8fF67E3A86B6dB964e26C875662ed55B77690'
+const FACTORY_ADDRESS_BASE = '0xF04f6ACf17C9e884D5eBE4aa6804cFD16CdEe32B'
 /**
  * To compute Pool address use DEPLOYER_ADDRESSES instead
  */
@@ -46,7 +46,7 @@ const FACTORY_ADDRESSES = {
 
 /** MasterChef V3 */
 const masterChefV3Addresses = {
-  [ChainId.MON_TESTNET]: '0x223eFB61a5f1c766A5fC7F637435251a91C356CA',
+  [ChainId.MON_TESTNET]: '0x49aE1Bb3C0d9bceE09611282A295aC0180494C04',
   [ChainId.AMOY_POLYGON]: '0x',
   [ChainId.BSC_TESTNET]: '0x3fFBCe2e4c0C023e43C1f529FC052a3eAb0Dc5A1',
   [ChainId.SEPOLIA]: '0x',
@@ -55,7 +55,7 @@ const masterChefV3Addresses = {
 
 /** Swap Router V3 */
 const swapRouterV3Addresses = {
-  [ChainId.MON_TESTNET]: '0xd3a075c688Ce2DCDef4C41D4CE88ceb5247B76A8',
+  [ChainId.MON_TESTNET]: '0x737A8fc40BCCa63dAE76BB1f300D31078fe9a79D',
   [ChainId.AMOY_POLYGON]: '0x',
   [ChainId.BSC_TESTNET]: '0xd4D2a2D6fdD9DD28a6B9f74c1d4C42F0FE9cc6BE',
   [ChainId.SEPOLIA]: '0x',
@@ -64,7 +64,7 @@ const swapRouterV3Addresses = {
 
 /** LiFi Contract Address */
 const lifiContractAddresses = {
-  [ChainId.MON_TESTNET]: '0xfFcB4378ef90F7e5508EAe3A85e6F31970105020',
+  [ChainId.MON_TESTNET]: '0x4797F967C3D77A1949Fb7F429f09072dFdB6de9d',
   [ChainId.AMOY_POLYGON]: '0x',
   [ChainId.BSC_TESTNET]: '0x03e078248A4cD85633273d905d4F026C68935d0f',
   [ChainId.SEPOLIA]: '0x',
@@ -73,7 +73,7 @@ const lifiContractAddresses = {
 
 /** multicall3 */
 const multicall3Addresses = {
-  [ChainId.MON_TESTNET]: '0xCaB7cFe801286f0cD82b0d9bd12b3Feb6CbDb18B'
+  [ChainId.MON_TESTNET]: '0x49C0e987D9E2a85FFf40664afc8C10420905826B'
 } as const satisfies Record<number, `0x${string}`>
 
 export default {
