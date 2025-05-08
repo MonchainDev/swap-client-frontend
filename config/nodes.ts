192dd7cd724e62f52f5f5a733e044367205af692
aniadev9 <aniadev.99@gmail.com>
2025-05-09 00:09:12 +0700

    chore: Update BSC Testnet node URL and package dependencies

--- config/nodes.ts
+++ config/nodes.ts
@@ -3,7 +3,7 @@ import { ChainId } from '~/types'
 
 export const PUBLIC_NODES: Record<ChainId, string[] | readonly string[]> = {
   [ChainId.MON_TESTNET]: ['https://testnet-rpc1.wicchain.com'],
-  [ChainId.BSC_TESTNET]: ['https://bsc-testnet-rpc.publicnode.com'],
+  [ChainId.BSC_TESTNET]: ['https://bsc-testnet.core.chainstack.com/529dab3b4d0890fa790c5ed475bcda6d'],
   [ChainId.SEPOLIA]: sepolia.rpcUrls.default.http,
   [ChainId.ARBITRUM_SEPOLIA]: arbitrumSepolia.rpcUrls.default.http,
   [ChainId.AMOY_POLYGON]: ['https://rpc-amoy.polygon.technology/']
